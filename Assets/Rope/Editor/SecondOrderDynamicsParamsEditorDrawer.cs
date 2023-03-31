c361205a61ce3b161cb3bf96177994cf32a9d5a2
Kovnir <akovnir@MacBook-Pro.local>
2023-03-31 23:18:30 +0300

    Fixed property drawer

--- Assets/Rope/Editor/SecondOrderDynamicsParamsEditorDrawer.cs
+++ Assets/Rope/Editor/SecondOrderDynamicsParamsEditorDrawer.cs
@@ -8,6 +8,9 @@ namespace Kovnir.Rope.Editor
     [CustomPropertyDrawer(typeof(SecondOrderCalculator.Params))]
     public sealed class SecondOrderDynamicsParamsEditorDrawer : PropertyDrawer
     {
+        private const int OFFSET = 2;
+        private const int GRAPH_SIZE = 200;
+
         static Material material;
         private MethodInfo manualUpdate;
 
@@ -27,41 +30,51 @@ namespace Kovnir.Rope.Editor
             }
         }
 
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return (EditorGUIUtility.singleLineHeight + OFFSET) * 3
+                   + EditorGUIUtility.singleLineHeight * 1.5f
+                   + GRAPH_SIZE;
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             LazyInitMaterial();
-            
+
             EditorGUI.BeginProperty(position, label, property);
+
             EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
             EditorGUI.indentLevel++;
-            SecondOrderCalculator.Consts consts = DrawMainProperties(property);
-            EditorGUI.indentLevel--;
 
-            EditorGUILayout.Space();
+            SecondOrderCalculator.Consts consts = DrawMainProperties(ref position, property);
 
-            Rect frameSize = GetCurrentRect();
-            frameSize.yMin = 0;
-            frameSize.yMax = 200;
-            Rect clipRect = GUILayoutUtility.GetRect(frameSize.xMin, frameSize.xMax, frameSize.yMin, frameSize.yMax);
+            position.y += EditorGUIUtility.singleLineHeight * 1.5f;
+            position.height = GRAPH_SIZE;
+
             if (Event.current.type == EventType.Repaint)
             {
-                DrawGraph(consts, material, clipRect, frameSize);
+                DrawGraph(consts, material, position);
             }
 
+            EditorGUI.indentLevel--;
 
             EditorGUI.EndProperty();
         }
 
-        private static SecondOrderCalculator.Consts DrawMainProperties(SerializedProperty property)
+        private static SecondOrderCalculator.Consts DrawMainProperties(ref Rect position, SerializedProperty property)
         {
             var frequency = property.FindPropertyRelative("Frequency");
             var damping = property.FindPropertyRelative("Damping");
             var response = property.FindPropertyRelative("Response");
 
-            EditorGUILayout.PropertyField(frequency);
-            EditorGUILayout.PropertyField(damping);
-            EditorGUILayout.PropertyField(response);
+            position.height = EditorGUIUtility.singleLineHeight;
+            position.y += EditorGUIUtility.singleLineHeight + OFFSET;
+            EditorGUI.PropertyField(position, frequency);
+            position.y += EditorGUIUtility.singleLineHeight + OFFSET;
+            EditorGUI.PropertyField(position, damping);
+            position.y += EditorGUIUtility.singleLineHeight + OFFSET;
+            EditorGUI.PropertyField(position, response);
 
             if (frequency.floatValue <= 0.1f)
             {
@@ -77,17 +90,7 @@ namespace Kovnir.Rope.Editor
                 new SecondOrderCalculator.Params(frequency.floatValue, damping.floatValue, response.floatValue));
         }
 
-        private static Rect GetCurrentRect()
-        {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            EditorGUILayout.EndHorizontal();
-            Rect frameSize = GUILayoutUtility.GetLastRect();
-            return frameSize;
-        }
-
-
-        private void DrawGraph(SecondOrderCalculator.Consts consts, Material material, Rect clipRect, Rect frameSize)
+        private void DrawGraph(SecondOrderCalculator.Consts consts, Material material, Rect clipRect)
         {
             const int BORDER = 3;
 
@@ -96,22 +99,22 @@ namespace Kovnir.Rope.Editor
             GL.Clear(true, false, Color.black);
             material.SetPass(0);
 
-            GLDraw.Rect(0, 0, frameSize.width, frameSize.yMax, Color.black);
-            GLDraw.EmptyRect(0, 0, frameSize.width, frameSize.yMax, BORDER, Color.gray);
+            GLDraw.Rect(0, 0, clipRect.width, clipRect.height, Color.black);
+            GLDraw.EmptyRect(0, 0, clipRect.width, clipRect.height, BORDER, Color.gray);
 
-            frameSize.width -= BORDER;
-            frameSize.y += BORDER;
-            frameSize.height -= BORDER * 2;
-            frameSize.x += BORDER;
+            clipRect.width -= BORDER;
+            clipRect.y += BORDER;
+            clipRect.height -= BORDER * 2;
+            clipRect.x += BORDER;
 
-            GLDraw.Line(BORDER, 100, frameSize.width, 100, new Color(0.7f, 0.7f, 0.7f));
+            GLDraw.Line(BORDER, 100, clipRect.width, 100, new Color(0.7f, 0.7f, 0.7f));
 
             const int THRESHOLD = 100;
 
             GLDraw.Lines(new Color(0.7f, 0.7f, 0), new Vector2(BORDER, 150), new Vector2(THRESHOLD, 150),
-                new Vector2(THRESHOLD, 50), new Vector2(frameSize.width, 50));
+                new Vector2(THRESHOLD, 50), new Vector2(clipRect.width, 50));
             GLDraw.Lines(new Color(0.7f, 0.7f, 0), new Vector2(BORDER, 150 + 1), new Vector2(THRESHOLD + 1, 150 + 1),
-                new Vector2(THRESHOLD + 1, 50 + 1), new Vector2(frameSize.width, 50 + 1));
+                new Vector2(THRESHOLD + 1, 50 + 1), new Vector2(clipRect.width, 50 + 1));
 
 
             Vector3 targetPosition = new(0, 1.5f, 0);
@@ -119,8 +122,8 @@ namespace Kovnir.Rope.Editor
             Vector3 currPosition = new(0, 1.5f, 0);
             Vector3 currVelocity = new(0, 0, 0);
 
-            Vector2[] points = new Vector2[(int)frameSize.width];
-            for (int i = 0; i < (int)frameSize.width; i++)
+            Vector2[] points = new Vector2[(int)clipRect.width];
+            for (int i = 0; i < (int)clipRect.width; i++)
             {
                 if (i > THRESHOLD)
                 {
@@ -128,14 +131,22 @@ namespace Kovnir.Rope.Editor
                 }
 
                 (currPosition, currVelocity) = ((Vector3, Vector3))manualUpdate
-                    .Invoke(null, new object[]{0.01f, targetPosition, prevTargetPosition,
-                        currPosition, currVelocity, consts});
+                    .Invoke(null, new object[]
+                    {
+                        0.01f, targetPosition, prevTargetPosition,
+                        currPosition, currVelocity, consts
+                    });
                 prevTargetPosition = targetPosition;
                 float currentPositionY = currPosition.y;
                 points[i] = new Vector2(BORDER + i, currentPositionY * 100);
             }
 
-            GLDraw.Lines(Color.green, frameSize, points);
+            Rect rect = clipRect;
+            rect.x = BORDER;
+            rect.y = BORDER;
+            rect.xMax = clipRect.width;
+            rect.yMax = GRAPH_SIZE - BORDER;
+            GLDraw.Lines(Color.green, rect, points);
 
             GL.PopMatrix();
             GUI.EndClip();
